e690d04486b17228bf45a80810433fda28543d15
noraananda <anandanora9@gmail.com>
2023-09-24 19:27:07 -0700

    push project to github

---
+++ operator/peru/perulangan/looping.c++
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+  for( i=1;i<16;i++)
+  {
+  cout<<i*i-i<<""<<endl;
+  }
+}
---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nama,alamat;
+    cout<<" ****************************"<<endl;
+    cout<<" *        BIODATA ANDA      *"<<endl;
+    cout<<"*****************************"<<endl;
+    cout<<"*   Masukkan nama anda =    *"<<endl;
+    cout<<"*****************************"<<endl;
+    cin>>nama;
+    cout<<"*****************************"<<endl;
+    cout<<"*  Masukkan alamat anda =   *"<<endl;
+    cout<<"*****************************"<<endl;
+    cin>>alamat;
+    cout<<"*****************************"<<endl;
+}
